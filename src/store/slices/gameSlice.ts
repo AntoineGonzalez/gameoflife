e8ef6c5d801db93d6f4bf3ba73d982baa1d347cf
AntoineGonzalez <45924026+AntoineGonzalez@users.noreply.github.com>
2022-09-14 09:58:01 +0200

    feature: compute & run iterations automatically

--- src/store/slices/gameSlice.ts
+++ src/store/slices/gameSlice.ts
@@ -28,16 +28,82 @@ const setCellIsAlive = (state: GameState, coord: Coord, value: boolean) => {
   }
 }
 
-export const initialState: GameState = {
+const initialState: GameState = {
   grid: [[]],
   isRunning: false,
   iterationCounter: 0
+}
+
+function getNeighboursValue (state: GameState, { x, y }: Coord): Boolean[] {
+  const gridWidth = state.grid[0].length
+  const gridHeight = state.grid.length
+
+  const northIndex = y > 0
+    ? y - 1
+    : gridHeight - 1
+  const southIndex = y < gridHeight - 1
+    ? y + 1
+    : 0
+  const westIndex = x > 0
+    ? x - 1
+    : gridWidth - 1
+  const eastIndex = x < gridHeight - 1
+    ? x + 1
+    : 0
+
+  return [
+    state.grid[northIndex][westIndex],
+    state.grid[northIndex][x],
+    state.grid[northIndex][eastIndex],
+    state.grid[y][westIndex],
+    state.grid[y][eastIndex],
+    state.grid[southIndex][westIndex],
+    state.grid[southIndex][x],
+    state.grid[southIndex][eastIndex]
+  ]
+}
+
+function computeIteration (state: GameState): GameState {
+  return {
+    ...state,
+    iterationCounter: state.iterationCounter + 1,
+    grid: state.grid.map((line, lineIndex) =>
+      line.map((cellIsAlive, colIndex) => {
+        const alivedNeighboursCount = getNeighboursValue(
+          state,
+          {
+            x: colIndex,
+            y: lineIndex
+          }
+        ).filter((value) => value).length
+
+        if ((!cellIsAlive && alivedNeighboursCount === 3) ||
+                    (cellIsAlive && [2, 3].includes(alivedNeighboursCount))
+        ) {
+          return true
+        }
+
+        return false
+      })
+    )
+  }
 }
 
 export const gameSlice = createSlice({
   name: 'game',
   initialState,
   reducers: {
+    tick: (state) => {
+      return {
+        ...computeIteration(state)
+      }
+    },
+    toggleRunningStatus: (state) => {
+      return {
+        ...state,
+        isRunning: !state.isRunning
+      }
+    },
     initialize: (state, action: PayloadAction<GridProps>) => {
       return {
         ...state,
@@ -56,10 +122,11 @@ export const gameSlice = createSlice({
 })
 
 export const {
-  initialize,
+  initialize, tick, toggleRunningStatus,
   killCell,
   reviveCell
 } = gameSlice.actions
 
 export const selectGrid: Selector<boolean[][]> = state => state.game.grid
+export const selectGameState: Selector<GameState> = state => state.game
 export default gameSlice.reducer
